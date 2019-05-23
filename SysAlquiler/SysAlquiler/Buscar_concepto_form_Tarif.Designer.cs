f15301d07e9de18722aa5acc881fe6a73a6f0e18
Esteban Vasquez Carrillo <44978702+Esteban96@users.noreply.github.com>
2019-05-23 14:36:11 -0500

    Add files via upload

---
+++ SysAlquiler/SysAlquiler/Buscar_concepto_form_Tarif.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SysAlquiler
+{
+    partial class Buscar_concepto_form_Tarif
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_concp = new System.Windows.Forms.DataGridView();
+            this.txt_buscanom = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_concp)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_concp
+            // 
+            this.dgv_concp.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.dgv_concp.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_concp.Location = new System.Drawing.Point(12, 113);
+            this.dgv_concp.Name = "dgv_concp";
+            this.dgv_concp.Size = new System.Drawing.Size(495, 325);
+            this.dgv_concp.TabIndex = 0;
+            // 
+            // txt_buscanom
+            // 
+            this.txt_buscanom.Location = new System.Drawing.Point(12, 71);
+            this.txt_buscanom.Name = "txt_buscanom";
+            this.txt_buscanom.Size = new System.Drawing.Size(245, 20);
+            this.txt_buscanom.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Buscar por Nombre de Concepto";
+            // 
+            // Buscar_concepto_form_Tarif
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(522, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_buscanom);
+            this.Controls.Add(this.dgv_concp);
+            this.Name = "Buscar_concepto_form_Tarif";
+            this.Opacity = 0.9D;
+            this.Text = "Buscar Concepto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_concp)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_concp;
+        private System.Windows.Forms.TextBox txt_buscanom;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ SysAlquiler/SysAlquiler/Reg_Tarifacs.Designer.cs
@@ -0,0 +1,293 @@
+﻿namespace SysAlquiler
+{
+    partial class Reg_Tarifacs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_buscarconcp = new System.Windows.Forms.Button();
+            this.txt_nom_concepto = new System.Windows.Forms.TextBox();
+            this.txt_id_concepto = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_id_tarifa = new System.Windows.Forms.TextBox();
+            this.txt_periodo = new System.Windows.Forms.TextBox();
+            this.txt_importe = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_buscar_tarifa = new System.Windows.Forms.Button();
+            this.cb_estado = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_guardar = new System.Windows.Forms.Button();
+            this.btn_modificar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_buscarconcp);
+            this.groupBox1.Controls.Add(this.txt_nom_concepto);
+            this.groupBox1.Controls.Add(this.txt_id_concepto);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(23, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(518, 60);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Datos por Concepto";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // btn_buscarconcp
+            // 
+            this.btn_buscarconcp.Location = new System.Drawing.Point(477, 21);
+            this.btn_buscarconcp.Name = "btn_buscarconcp";
+            this.btn_buscarconcp.Size = new System.Drawing.Size(35, 23);
+            this.btn_buscarconcp.TabIndex = 3;
+            this.btn_buscarconcp.Text = "...";
+            this.btn_buscarconcp.UseVisualStyleBackColor = true;
+            this.btn_buscarconcp.Click += new System.EventHandler(this.btn_buscarconcp_Click);
+            // 
+            // txt_nom_concepto
+            // 
+            this.txt_nom_concepto.Location = new System.Drawing.Point(243, 23);
+            this.txt_nom_concepto.Name = "txt_nom_concepto";
+            this.txt_nom_concepto.ReadOnly = true;
+            this.txt_nom_concepto.Size = new System.Drawing.Size(210, 20);
+            this.txt_nom_concepto.TabIndex = 2;
+            // 
+            // txt_id_concepto
+            // 
+            this.txt_id_concepto.Location = new System.Drawing.Point(77, 23);
+            this.txt_id_concepto.Name = "txt_id_concepto";
+            this.txt_id_concepto.ReadOnly = true;
+            this.txt_id_concepto.Size = new System.Drawing.Size(52, 20);
+            this.txt_id_concepto.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre Concepto:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Id Concepto:";
+            // 
+            // txt_id_tarifa
+            // 
+            this.txt_id_tarifa.Location = new System.Drawing.Point(72, 31);
+            this.txt_id_tarifa.Name = "txt_id_tarifa";
+            this.txt_id_tarifa.ReadOnly = true;
+            this.txt_id_tarifa.Size = new System.Drawing.Size(52, 20);
+            this.txt_id_tarifa.TabIndex = 1;
+            // 
+            // txt_periodo
+            // 
+            this.txt_periodo.Location = new System.Drawing.Point(72, 75);
+            this.txt_periodo.Name = "txt_periodo";
+            this.txt_periodo.Size = new System.Drawing.Size(52, 20);
+            this.txt_periodo.TabIndex = 2;
+            // 
+            // txt_importe
+            // 
+            this.txt_importe.Location = new System.Drawing.Point(72, 121);
+            this.txt_importe.Name = "txt_importe";
+            this.txt_importe.Size = new System.Drawing.Size(60, 20);
+            this.txt_importe.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_buscar_tarifa);
+            this.groupBox2.Controls.Add(this.cb_estado);
+            this.groupBox2.Controls.Add(this.txt_importe);
+            this.groupBox2.Controls.Add(this.txt_periodo);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.txt_id_tarifa);
+            this.groupBox2.Location = new System.Drawing.Point(23, 83);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(518, 207);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos de Tarifa";
+            // 
+            // btn_buscar_tarifa
+            // 
+            this.btn_buscar_tarifa.Location = new System.Drawing.Point(349, 146);
+            this.btn_buscar_tarifa.Name = "btn_buscar_tarifa";
+            this.btn_buscar_tarifa.Size = new System.Drawing.Size(158, 38);
+            this.btn_buscar_tarifa.TabIndex = 5;
+            this.btn_buscar_tarifa.Text = "Buscar";
+            this.btn_buscar_tarifa.UseVisualStyleBackColor = true;
+            this.btn_buscar_tarifa.Click += new System.EventHandler(this.btn_buscar_tarifa_Click);
+            // 
+            // cb_estado
+            // 
+            this.cb_estado.FormattingEnabled = true;
+            this.cb_estado.Location = new System.Drawing.Point(386, 30);
+            this.cb_estado.Name = "cb_estado";
+            this.cb_estado.Size = new System.Drawing.Size(121, 21);
+            this.cb_estado.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(337, 33);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Estado:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(21, 124);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 13);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Importe:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Periodo:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(17, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Id Tarifa:";
+            // 
+            // btn_guardar
+            // 
+            this.btn_guardar.Location = new System.Drawing.Point(23, 314);
+            this.btn_guardar.Name = "btn_guardar";
+            this.btn_guardar.Size = new System.Drawing.Size(75, 23);
+            this.btn_guardar.TabIndex = 5;
+            this.btn_guardar.Text = "Guardar";
+            this.btn_guardar.UseVisualStyleBackColor = true;
+            this.btn_guardar.Click += new System.EventHandler(this.btn_guardar_Click);
+            // 
+            // btn_modificar
+            // 
+            this.btn_modificar.Location = new System.Drawing.Point(170, 314);
+            this.btn_modificar.Name = "btn_modificar";
+            this.btn_modificar.Size = new System.Drawing.Size(75, 23);
+            this.btn_modificar.TabIndex = 5;
+            this.btn_modificar.Text = "Modificar";
+            this.btn_modificar.UseVisualStyleBackColor = true;
+            this.btn_modificar.Click += new System.EventHandler(this.btn_modificar_Click);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(466, 314);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(75, 23);
+            this.btn_limpiar.TabIndex = 5;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Location = new System.Drawing.Point(328, 314);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(75, 23);
+            this.btn_eliminar.TabIndex = 5;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            // 
+            // Reg_Tarifacs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SlateGray;
+            this.ClientSize = new System.Drawing.Size(565, 349);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_modificar);
+            this.Controls.Add(this.btn_guardar);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Reg_Tarifacs";
+            this.Opacity = 0.9D;
+            this.Text = "Registro de Tarifa";
+            this.Load += new System.EventHandler(this.Reg_Tarifacs_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_buscarconcp;
+        private System.Windows.Forms.TextBox txt_nom_concepto;
+        private System.Windows.Forms.TextBox txt_id_concepto;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_id_tarifa;
+        private System.Windows.Forms.TextBox txt_periodo;
+        private System.Windows.Forms.TextBox txt_importe;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_buscar_tarifa;
+        private System.Windows.Forms.ComboBox cb_estado;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_guardar;
+        private System.Windows.Forms.Button btn_modificar;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_eliminar;
+    }
+}
